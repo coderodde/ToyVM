04276d0168cddaf6492fa64c88d6d3d3d1155a5a
Rodion Efremov <rodion.efremov@helsinki.fi>
2016-03-09 18:23:59 +0200

    Debuggin FizzBuzz.

--- main.c
+++ main.c
@@ -5,41 +5,213 @@ int main(int argc, const char * argv[]) {
     TOYVM vm;
     INIT_VM(&vm, 1024, 512);
     
-    /* REG4 holds the maximum allowed loop counter value. */
+    // CONST REG4 100
     vm.memory[0] = CONST;
     vm.memory[1] = REG4;
     WRITE_WORD(&vm, 2, 100);
     
-    /* REG2 holds the value one (1) for incrementing the value of loop 
-       couter. */
+    // CONST REG2 1
     vm.memory[6] = CONST;
     vm.memory[7] = REG2;
     WRITE_WORD(&vm, 8, 1);
     
-    /* Increment REG1 by one. */
+    // [12] ADD REG2 REG1
     vm.memory[12] = ADD;
     vm.memory[13] = REG2;
     vm.memory[14] = REG1;
     
-    /* Compare the value of REG1 and REG4. */
+    // CMP REG1 REG4
     vm.memory[15] = CMP;
     vm.memory[16] = REG1;
     vm.memory[17] = REG4;
-    /* If done, jump to a HALT instruction. */
+    
+    // JA 200
     vm.memory[18] = JA;
-    WRITE_WORD(&vm, 19, 100);
+    WRITE_WORD(&vm, 19, 200);
     
-    /* Increment REG3. */
-    vm.memory[23] = ADD;
-    vm.memory[24] = REG2;
-    vm.memory[25] = REG3;
-    vm.memory[26] = JMP;
-    WRITE_WORD(&vm, 27, 12);
+    // PUSH_ALL
+    vm.memory[23] = PUSH_ALL;
     
-    vm.memory[100] = HALT;
+    // CALL 50
+    vm.memory[24] = CALL;
+    WRITE_WORD(&vm, 25, 50);
+    
+    // POP_ALL
+    vm.memory[29] = POP_ALL;
+    
+    // JMP
+    vm.memory[30] = JMP;
+    WRITE_WORD(&vm, 31, 12);
+    
+//[50] CONST REG2 15
+    vm.memory[50] = CONST;
+    vm.memory[51] = REG2;
+    WRITE_WORD(&vm, 52, 15);
+    
+    // MOD REG1 REG2
+    vm.memory[56] = MOD;
+    vm.memory[57] = REG1;
+    vm.memory[58] = REG2;
+    
+    // CONST REG3 0
+    vm.memory[59] = CONST;
+    vm.memory[60] = REG3;
+    WRITE_WORD(&vm, 61, 0);
+    
+    // CMP REG2 REG3
+    vm.memory[65] = CMP;
+    vm.memory[66] = REG2;
+    vm.memory[67] = REG3;
+    
+    // JB 93
+    vm.memory[68] = JB;
+    WRITE_WORD(&vm, 69, 93);
+    
+    // JA 93
+    vm.memory[73] = JA;
+    WRITE_WORD(&vm, 74, 93);
+    
+    // CONST REG3 300
+    vm.memory[78] = CONST;
+    vm.memory[79] = REG3;
+    WRITE_WORD(&vm, 80, 300);
+    
+    // PUHS REG3
+    vm.memory[84] = PUSH;
+    vm.memory[85] = REG3;
+    
+    // INT 2
+    vm.memory[86] = INT;
+    vm.memory[87] = 2;
+    
+    // JMP 193
+    vm.memory[88] = JMP;
+    WRITE_WORD(&vm, 89, 193);
+    
+////////////////////////////
+//[93] CONST REG2 5
+    vm.memory[93] = CONST;
+    vm.memory[94] = REG2;
+    WRITE_WORD(&vm, 95, 5);
+    
+    // MOD REG1 REG2
+    vm.memory[99] = MOD;
+    vm.memory[100] = REG1;
+    vm.memory[101] = REG2;
+    
+    // CONST REG3 0
+    vm.memory[102] = CONST;
+    vm.memory[103] = REG3;
+    WRITE_WORD(&vm, 104, 0);
+    
+    // CMP REG2 REG3
+    vm.memory[108] = CMP;
+    vm.memory[109] = REG2;
+    vm.memory[110] = REG3;
+    
+    // JB 136
+    vm.memory[111] = JB;
+    WRITE_WORD(&vm, 112, 136);
+    
+    // JA 136
+    vm.memory[116] = JA;
+    WRITE_WORD(&vm, 117, 136);
+    
+    // CONST REG3 300
+    vm.memory[121] = CONST;
+    vm.memory[122] = REG3;
+    WRITE_WORD(&vm, 123, 300);
+    
+    // PUHS REG3
+    vm.memory[127] = PUSH;
+    vm.memory[128] = REG3;
+    
+    // INT 2
+    vm.memory[129] = INT;
+    vm.memory[130] = 2;
+    
+    // JMP 193
+    vm.memory[131] = JMP;
+    WRITE_WORD(&vm, 132, 193);
+    
+//////////////////////////
+//[136] CONST REG2 3
+    vm.memory[136] = CONST;
+    vm.memory[137] = REG2;
+    WRITE_WORD(&vm, 138, 3);
+    
+    // MOD REG1 REG2
+    vm.memory[142] = MOD;
+    vm.memory[143] = REG1;
+    vm.memory[144] = REG2;
+    
+    // CONST REG3 0
+    vm.memory[145] = CONST;
+    vm.memory[146] = REG3;
+    WRITE_WORD(&vm, 147, 0);
+    
+    // CMP REG2 REG3
+    vm.memory[151] = CMP;
+    vm.memory[152] = REG2;
+    vm.memory[153] = REG3;
+    
+    // JB 179
+    vm.memory[154] = JB;
+    WRITE_WORD(&vm, 155, 179);
+    
+    // JA 179
+    vm.memory[159] = JA;
+    WRITE_WORD(&vm, 160, 179);
+    
+    // CONST REG3 300
+    vm.memory[164] = CONST;
+    vm.memory[165] = REG3;
+    WRITE_WORD(&vm, 166, 300);
+    
+    // PUHS REG3
+    vm.memory[170] = PUSH;
+    vm.memory[171] = REG3;
+    
+    // INT 2
+    vm.memory[172] = INT;
+    vm.memory[173] = 2;
+    
+    // JMP 193
+    vm.memory[174] = JMP;
+    WRITE_WORD(&vm, 175, 193);
+    ////
+    
+//[179]: PUSH REG1
+    vm.memory[179] = PUSH;
+    vm.memory[180] = REG1;
+    
+    // INT 1
+    vm.memory[181] = INT;
+    vm.memory[182] = INTERRUPT_PRINT_INTEGER;
+    
+    // CONST REG4 350
+    vm.memory[183] = CONST;
+    vm.memory[184] = REG4;
+    WRITE_WORD(&vm, 185, 350);
+    
+    // PUSH REG4
+    vm.memory[189] = PUSH;
+    vm.memory[190] = REG4;
+    
+    // INT INTEGER
+    vm.memory[191] = INT;
+    vm.memory[192] = INTERRUPT_PRINT_STRING;
+    
+//[193]:
+    vm.memory[193] = RET;
+    vm.memory[200] = HALT;
+    
+    memcpy(&vm.memory[300], "FizzBuzz\n", strlen("FizzBuzz\n"));
+    memcpy(&vm.memory[320], "Fizz\n", strlen("Fizz\n"));
+    memcpy(&vm.memory[340], "Buzz\n", strlen("Buzz\n"));
+    memcpy(&vm.memory[350], "\n", strlen("\n"));
+    
     RUN_VM(&vm);
-    
-    printf("REG3: %d.\n", vm.cpu.registers[REG3]);
     PRINT_STATUS(&vm);
     return 0;
 }
